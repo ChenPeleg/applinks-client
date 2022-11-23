bc43e1d4f928dbfd3cebd255556e38b9373419a8
chenp <chenpeleg@gmail.com>
2022-11-23 12:50:54 +0200

    added get and set buttons

--- examples/main.js
+++ examples/main.js
@@ -5,7 +5,26 @@ const appLinkClient = new APPLinksClient('my-app');
 const loginToServer = async () => {
     const result = /** @type { UserData }*/ await appLinkClient.LoginThroughAppLinks();
     const userState = document.body.querySelector('div.user-state');
-    userState.innerHTML = `${result.firstName} ${result.lastName} `;
+    userState.innerHTML = `Hello, ${result.firstName} ${result.lastName} `;
+
+    const buttons = document.body.querySelectorAll('button.not-active');
+    buttons.forEach(btn => {
+
+
+        btn.classList.remove('not-active');
+    })
+};
+const loadData = async () => {
+    const result = /** @type { UserData }*/ await appLinkClient.LoginThroughAppLinks();
+    const userState = document.body.querySelector('div.user-state');
+    userState.innerHTML = `Hello, ${result.firstName} ${result.lastName} `;
+};
+const saveData = async () => {
+    const result = /** @type { UserData }*/ await appLinkClient.LoginThroughAppLinks();
+    const userState = document.body.querySelector('div.user-state');
+    userState.innerHTML = `Hello, ${result.firstName} ${result.lastName} `;
 };
 
 window.loginToServer = loginToServer;
+window.loadData = loadData;
+window.saveData = saveData;
