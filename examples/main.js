e58513469f8d3f78e76ea73be58dfdc0c02726c7
chenp <chenpeleg@gmail.com>
2022-11-25 13:47:19 +0200

    added the record id

--- examples/main.js
+++ examples/main.js
@@ -1,6 +1,6 @@
 import { APPLinksClient } from '../src/client.js';
 
-const appLinkClient = new APPLinksClient('my-demo-app');
+const appLinkClient = new APPLinksClient('my-demo-app2');
 
 const updateUserUi = (userData /** @type { UserData }*/) => {
     const userState = document.body.querySelector('div.user-state');
