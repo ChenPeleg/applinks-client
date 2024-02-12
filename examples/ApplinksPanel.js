a623033a785c13501f1e2b3abf5dd4f43dedde14
Chen Peleg <chenpeleg@gmail.com>
2024-02-12 10:10:13 +0200

    fixed all issues in file

--- examples/ApplinksPanel.js
+++ examples/ApplinksPanel.js
@@ -1,3 +1,14 @@
+//@ts-check
+
+class ApplinksPanelOptionsGraphicUtils {
+    static userIcon = `<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M480-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM160-160v-112q0-34 17.5-62.5T224-378q62-31 126-46.5T480-440q66 0 130 15.5T736-378q29 15 46.5 43.5T800-272v112H160Zm80-80h480v-32q0-11-5.5-20T700-306q-54-27-109-40.5T480-360q-56 0-111 13.5T260-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T560-640q0-33-23.5-56.5T480-720q-33 0-56.5 23.5T400-640q0 33 23.5 56.5T480-560Zm0-80Zm0 400Z"/></svg>`;
+}
+
+/**
+ * @typedef UserData
+ * @property { string } username
+ *  @property  { string } fullName
+ */
 class ApplinksPanelOptions {
     static Position = {
         bottomLeft: 'bottomLeft',
@@ -9,7 +20,20 @@ class ApplinksPanelOptions {
         classic: 'classic',
     };
 
-    constructor({ panelType, position } = {}) {
+    /**
+     * @constructor
+
+     * @param {{position :keyof ApplinksPanelOptions.Position,
+     *  panelType: keyof ApplinksPanelOptions.PanelType
+     * }} props
+     */
+    constructor(
+        { panelType, position } = {
+            // @ts-ignore
+            panelType: ApplinksPanelOptions.PanelType.classic, // @ts-ignore
+            position: ApplinksPanelOptions.Position.bottomLeft,
+        }
+    ) {
         this.position = position || ApplinksPanelOptions.Position.bottomLeft;
         this.panelType = panelType || ApplinksPanelOptions.PanelType.classic;
     }
@@ -21,9 +45,12 @@ export class ApplinksPanel {
     /** @type {HTMLDivElement}     */
     #panelElement = null;
 
+    /**
+     * @param {ApplinksPanelOptions} [panelOptions]
+     */
     constructor(panelOptions) {
         this.panelOptions = panelOptions || new ApplinksPanelOptions();
-        console.log('applinks panel');
+
         this.#panelElement = this.#createPanelElement();
         document.body.appendChild(this.#panelElement);
     }
@@ -37,10 +64,18 @@ export class ApplinksPanel {
         // element.style.height = '100px';
 
         const innerHtml = `
-        <div style="width: 100px; height: 100px; background-color: red; position: fixed; top:0">
+        <div style="width: 40px; height: 40px; background-color: red; position: fixed; top:0">
+        <div id="${this.#applinksPanelId}-main-user-button">
+        <div id="${this.#applinksPanelId}-main-user-icon" style="cursor: pointer; 
+        display: flex; justify-content: center; align-items: center;
+        border-radius: 50%; background-color: aliceblue;width: 40px; height: 40px">
+        ${ApplinksPanelOptionsGraphicUtils.userIcon}
+        </div>
         
+        </div>
         </div>`;
         element.innerHTML = innerHtml;
+
         return element;
     }
 }
