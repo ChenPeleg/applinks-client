016a92fd94824ca09bfe1d65f15232b71af39cda
Chen Peleg <chenpeleg@gmail.com>
2024-02-13 07:05:39 +0200

    showing popover

--- examples/ApplinksPanel.js
+++ examples/ApplinksPanel.js
@@ -12,6 +12,9 @@ class ApplinksPanelOptionsGraphicUtils {
 <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M360-390q-21 0-35.5-14.5T310-440q0-21 14.5-35.5T360-490q21 0 35.5 14.5T410-440q0 21-14.5 35.5T360-390Zm240 0q-21 0-35.5-14.5T550-440q0-21 14.5-35.5T600-490q21 0 35.5 14.5T650-440q0 21-14.5 35.5T600-390ZM480-160q134 0 227-93t93-227q0-24-3-46.5T786-570q-21 5-42 7.5t-44 2.5q-91 0-172-39T390-708q-32 78-91.5 135.5T160-486v6q0 134 93 227t227 93Zm0 80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm-54-715q42 70 114 112.5T700-640q14 0 27-1.5t27-3.5q-42-70-114-112.5T480-800q-14 0-27 1.5t-27 3.5ZM177-581q51-29 89-75t57-103q-51 29-89 75t-57 103Zm249-214Zm-103 36Z"/></svg>`;
 
     static getCss = (id) => `
+    :root {
+       --popover-transition-duration: 0.2s;
+    }
     #${id}-wrapper {
         width: 40px;
         height: 40px;
@@ -56,6 +59,50 @@ class ApplinksPanelOptionsGraphicUtils {
     #${id}-unloged-user {
         
     }
+    #${id}-popover {
+        border-radius: 5px;
+        box-shadow: 0 0 5px 2px rgba(0, 0, 0, 0.2);
+        border: 0px solid rgba(0, 0, 0, 0.2);
+        background-color: #FAF9F6;
+    }
+    #${id}-popover-close {
+    
+    }
+    
+    
+#${id}-popover[popover]:popover-open {
+    opacity: 1;
+    transform: scale(1);
+}
+
+#${id}-popover[popover] {
+
+
+    /* Final state of the exit animation */
+    opacity: 0;
+    transform: scale(.8);
+
+    transition:
+            opacity var(--popover-transition-duration),
+            transform var(--popover-transition-duration),
+            overlay var(--popover-transition-duration) allow-discrete,
+            visibility var(--popover-transition-duration) allow-discrete,
+            display var(--popover-transition-duration) allow-discrete;
+    /* Equivalent to
+    transition: all 0.7s allow-discrete; */
+
+
+    }
+
+   /* Needs to be after the previous .popoverClass[popover]:popover-open rule
+    to take effect, as the specificity is the same */
+   @starting-style {
+    #${id}-popover[popover]:popover-open {
+        opacity: 0;
+        transform: scale(0);
+    }
+    }
+
     `;
 }
 
@@ -111,7 +158,7 @@ export class ApplinksPanel {
         this.panelOptions = panelOptions || new ApplinksPanelOptions();
 
         this.#panelElement = this.#createPanelElement();
-        document.body.appendChild(this.#panelElement);
+
         this.#addCSS(ApplinksPanelOptionsGraphicUtils.getCss(this.#applinksPanelId));
     }
 
@@ -175,14 +222,17 @@ export class ApplinksPanel {
         const element = document.createElement('div');
 
         const innerHtml = `
-        <dialog>
-          <button autofocus>Close</button>
+        <div popover id="${this.#applinksPanelId}-popover"  >
+        <div role="button" autofocus id="${this.#applinksPanelId}-popover-close">Close</div>
+        
+        <p>Logged in as <span id="${this.#applinksPanelId}-popover-login-name"></span> </p>
+        
           <p>This modal dialog has a groovy backdrop!</p>
-        </dialog>
+        </div>
       
 
         <div id="${this.#applinksPanelId}-wrapper" >
-        <div id="${this.#applinksPanelId}-main-user-button">
+        <div id="${this.#applinksPanelId}-main-user-button" aria-haspopup="true" popovertarget="${this.#applinksPanelId}-popover">
             <div id="${this.#applinksPanelId}-unloged-user"  
             class="${this.#applinksPanelId}-main-icon active" style="display: flex">
             ${ApplinksPanelOptionsGraphicUtils.userIcon}
@@ -207,20 +257,43 @@ export class ApplinksPanel {
         </div>
         </div>`;
         element.innerHTML = innerHtml;
-        element.addEventListener("click", (ev) => {
-            this.#clickHandler(ev);
-        });
+        document.body.appendChild(element);
+        this.#attachHandlers();
 
         return element;
     }
 
-    #clickHandler = (ev) => {
-        if (this.#status === 'not-logged-in') {
+    #attachHandlers = () => {
+        const mainElement = document.querySelector(`#${this.#applinksPanelId}-wrapper`);
+        const popover = document.querySelector(`#${this.#applinksPanelId}-popover`);
 
-        }
-        console.log('click', ev);
-        // dialog.showModal();
+        mainElement.addEventListener("click", (ev) => {
+            if (this.#status === 'not-logged-in') {
+                // @ts-ignore
+                popover.showPopover();
+            }
+            // @ts-ignore
+            popover.showPopover();
+            console.log('click', ev);
+        });
+        popover.addEventListener("close", (ev) => {
+            console.log('close', ev);
+        });
+        document.querySelector(`#${this.#applinksPanelId}-popover-close`)
+            .addEventListener("click", (ev) => {
+                // @ts-ignore
+                popover.hidePopover();
+            });
+
+        setTimeout(() => {
+            // @ts-ignore
+            popover.showPopover();
+        }, 100);
+
+
     };
+
+
 }
 
 // const panel = new ApplinksPanel();
